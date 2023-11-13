d96a453a9ea6d1f896a68e89e35a474cb8fab79d
ManuelVaronG <manueldavidgil@gmail.com>
2023-11-13 16:34:13 -0500

    Actu

--- Main.py
+++ Main.py
@@ -9,7 +9,12 @@ def Calculo_Coaxial( parametros : Models.Parametros_Coaxial):
     valores_graf=[]
     print(parametros.a, parametros.b,parametros.l, parametros.f, parametros.sigma, parametros.Er, parametros.μr)
     R,L,C,G,w= Calculos.Resultado_Coaxial(parametros.a, parametros.b, parametros.f, parametros.sigma, parametros.Er, parametros.μr)
+    R=float("{:.3e}".format(R))
+    L=float("{:.3e}".format(L))
+    C=float("{:.3e}".format(C))
+    G=float("{:.3e}".format(G))
     alfa, floss=Calculos.Perdidas_Cable(R,L,C,G,parametros.l,w)
+    alfa=float("{:.3e}".format(alfa))
     valores_graf=Calculos.Valores_a_Graficar(alfa, parametros.l)
     return {'R' : R, 'L' : L, 'G' : G, 'C' : C, 'alfa' : alfa, 'floss' : floss, 'Val_Graf' : valores_graf}
 
@@ -18,7 +23,11 @@ def Calculo_Fibra( parametros : Models.Parametros_Fibra):
     
     print(parametros.n, parametros.c, parametros.e, parametros.J, parametros.A, parametros.L)
     TA, OA, CA, FA= Calculos.Resultado_Fibra(parametros.n, parametros.c, parametros.e, parametros.J, parametros.A, parametros.L)
-
+    TA=float("{:.3e}".format(TA))
+    OA=float("{:.3e}".format(OA))
+    CA=float("{:.3e}".format(CA))
+    FA=float("{:.3e}".format(FA))
+    
     return {'TA': TA, 'OA': OA, 'CA': CA, 'FA': FA}
 
 
